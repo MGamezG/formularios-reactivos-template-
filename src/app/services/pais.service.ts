dc0807da07704880be17f39849500f42200f8d4a
MGamezG <l171130314@gamadero.tecnm.mx>
2022-06-25 10:25:01 -0500

    template: api de paises

---
+++ src/app/services/pais.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  constructor(private http: HttpClient) { }
+
+  getPaises(){
+    return this.http.get(`https://restcountries.com/v3.1/lang/spa`).pipe(map((resp:any)=>{
+      return resp.map((pais:any)=>{
+        return {
+          nombre: pais.name.official,
+          codigo: pais.cca3
+        }
+      })
+    }));
+  }
+}
