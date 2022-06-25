1d0099444e70ef80a495e15245df0e185b183d82
MGamezG <l171130314@gamadero.tecnm.mx>
2022-06-25 09:51:28 -0500

    template:mostrar errores

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./template.component.scss']
 })
 export class TemplateComponent implements OnInit {
+  user={
+    nombre:'miguel',
+    correo:'mga@gmail.con',
+    apellido:'gamez'
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  guardar(forma:NgForm){
+    console.log(forma);
+    if (forma.invalid) {
+      Object.values(forma.controls).forEach(control=>{
+        console.warn("en el control");
+        control.markAllAsTouched();
+      });
+      return;
+    }
+    console.log(forma.value);
+  }
+
 
 }
