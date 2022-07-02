8f52d01fa39de6b9d8cf84bf58386b45bb524e5b
MGamezG <l171130314@gamadero.tecnm.mx>
2022-07-02 15:47:08 -0500

    Reactive: creacion de formularios-FormGroup

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive.component.scss']
 })
 export class ReactiveComponent implements OnInit {
+  // forma:FormGroup;
+  forma:any;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit(): void {
+  }
+  createForm(){
+    this.forma=this.fb.group({
+      nombre:[''],
+      apellido:[''],
+      correo:[''],
+    })
+  }
+  save(){
+    console.log(this.forma)
   }
 
 }
