b25a2274cde27d0e90928be2c5b329909c61c9ce
MGamezG <l171130314@gamadero.tecnm.mx>
2022-07-04 18:41:24 -0500

    Reactive: detectar cambios en los valores, estado del formulario controles

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -8,89 +8,140 @@ import { ValidadoresService } from 'src/app/services/validadores.service';
   styleUrls: ['./reactive.component.scss']
 })
 export class ReactiveComponent implements OnInit {
-  // forma:FormGroup;
-  forma!: FormGroup;
+  forma: FormGroup;
 
-  constructor(private fb: FormBuilder, private validadores:ValidadoresService) {
-    this.createForm();
-    this.chargeData();
-   }
+
+  constructor( private fb: FormBuilder,
+               private validadores: ValidadoresService ) {
+
+    this.crearFormulario();
+    this.cargarDataAlFormulario();
+    this.crearListeners();
+
+  }
 
   ngOnInit(): void {
   }
-  get nameNotValid(){
-    return this.forma.get('nombre')?.invalid && this.forma.get('nombre')?.touched
-  }
-  get surnameNotValid(){
-    return this.forma.get('apellido')?.invalid && this.forma.get('apellido')?.touched
-  }
-  get emailNotValid(){
-    return this.forma.get('correo')?.invalid && this.forma.get('correo')?.touched
-  }
-  get districtNotValid(){
-    return this.forma.get('direccion.distrito')?.invalid && this.forma.get('direccion.distrito')?.touched
-  }
-  get cityNotValid(){
-    return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
-  }
-  get pasatiempos(){
+
+  get pasatiempos() {
     return this.forma.get('pasatiempos') as FormArray;
   }
 
-  createForm(){
-    this.forma=this.fb.group({
-      nombre:['',[Validators.required,Validators.minLength(5),this.validadores.noMiguel ]],//validadores sincronos, se pueden hacer inmediatamente sin requeri de servicios web
-      apellido:['',[Validators.required,Validators.minLength(5) ]],
-      correo:['',[Validators.required ,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-      direccion:this.fb.group({
-        distrito:['',Validators.required],
-        ciudad:['',Validators.required]
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched
+  }
+
+  get usuarioNoValido() {
+    return this.forma.get('usuario').invalid && this.forma.get('usuario').touched
+  }
+
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
+  }
+
+  get pass1NoValido() {
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched;
+  }
+
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return ( pass1 === pass2 ) ? false : true;
+  }
+
+
+
+  crearFormulario() {
+
+    this.forma = this.fb.group({
+      nombre  : ['', [ Validators.required, Validators.minLength(5) ]  ],
+      apellido: ['', [Validators.required, this.validadores.noGamez ] ],
+      correo  : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      usuario : ['', , this.validadores.existeUsuario ],
+      pass1   : ['', Validators.required ],
+      pass2   : ['', Validators.required ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required ],
+        ciudad  : ['', Validators.required ],
       }),
-     pasatiempos:this.fb.array([])
+      pasatiempos: this.fb.array([])
+    },{
+      validators: this.validadores.passwordsIguales('pass1','pass2')
     });
+
   }
-  chargeData(){
-  //   this.forma.setValue({
-  //     nombre:'miguel',
-  //     apellido:'gamez',
-  //     correo: "practicante@gta.ai",
-  //     direccion: {
-  //     distrito: "fsdfsdfs",
-  //     ciudad: "fsadfas"
-  //          }
-  //   });
-  this.forma.setValue({
-    nombre:'miguel',
-    apellido:'gamez',
-    correo: "practicante@gta.ai",
-    direccion: {
-    distrito: "fsdfsdfs",
-    ciudad: "fsadfas"
-              }
-         });
+
+  crearListeners() {
+    // this.forma.valueChanges.subscribe( valor => {
+    //   console.log(valor);
+    // });
+
+    // this.forma.statusChanges.subscribe( status => console.log({ status }));
+    this.forma.get('nombre').valueChanges.subscribe( console.log );
   }
-  addPasatiempo(){
-    this.pasatiempos.push(this.fb.control(''));
+
+  cargarDataAlFormulario() {
+
+    // this.forma.setValue({
+    this.forma.reset({
+      nombre: 'Miguel',
+      apellido: 'Gamez',
+      correo: 'Mgamez@gmail.com',
+      pass1: '12345',
+      pass2: '12345',
+      direccion: {
+        distrito: 'ecatepec',
+        ciudad: 'Mexico'
+      },
+    });
+
   }
-  deletePasatiempo(i:number){
+
+
+  agregarPasatiempo() {
+    this.pasatiempos.push(  this.fb.control('')  );
+  }
+
+  borrarPasatiempo(i: number) {
     this.pasatiempos.removeAt(i);
   }
-  save(){
-    console.log(this.forma)
-    if (this.forma.invalid) {
-      return Object.values(this.forma.controls).forEach(control => {
-        if(control instanceof FormGroup){
-          Object.values(control.controls).forEach(control=>control.markAllAsTouched());
-        }else{
-          control.markAllAsTouched();
+
+
+  guardar() {
+    console.log( this.forma );
+
+    if ( this.forma.invalid ) {
+
+      return Object.values( this.forma.controls ).forEach( control => {
+
+        if ( control instanceof FormGroup ) {
+          Object.values( control.controls ).forEach( control => control.markAsTouched() );
+        } else {
+          control.markAsTouched();
         }
-        console.warn("en el control");
+
 
       });
+
     }
-    // this.forma.reset({
 
-    // });
+    // Posteo de información
+    this.forma.reset({
+      nombre: 'Sin nombre'
+    });
+
   }
-
 }
